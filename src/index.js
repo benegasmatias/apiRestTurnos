c1e25df117067193818afddfc3064e4245683b0b
unknown <benegas.matias21@gmail.com>
2020-02-11 09:20:19 -0300

    compression()

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app=express();
 const cors = require('cors');
 //SETTING
 app.set('port',process.env.PORT||3000);
-
+app.use(compression());
 //middlewares
 
 app.use(express.json());//se puede accceder a la infromacion gracias a esta linea de codigo
@@ -15,7 +15,7 @@ app.use(require('./routes/personas'));
 app.use(require('./routes/vehiculos'));
 app.use(require('./routes/sectores'));
 app.use(require('./routes/turnos'));
-app.use(compression());
+
 app.listen(app.get('port'), ()=>{
     console.log('server on port ',app.get('port'));
 })
